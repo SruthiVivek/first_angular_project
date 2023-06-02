048f0b1b7ede053b7ea196adfe71fad7d4c480d2
unknown <sruthimohanan96@gmail.com>
2023-06-01 23:38:57 -0400

    bank angular

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from "../DataService";
 
 @Component({
   selector: 'app-login',
@@ -9,9 +11,20 @@ export class LoginComponent {
 
   data="happy banking with us"
   pdata="ACC NO"
+  DataService:any
+  constructor(private rout:Router,private ds: DataService){
+
+  }
+  ngOnInit():void {
+    this.DataService=this.ds.sdata
+    console.log(this.DataService);
+    this.ds.smethod()
+    
+  }
 
   login(){
-    alert(`login working`)
+    // alert(`login working`)
+    this.rout.navigateByUrl('home')
   }
   acnochange(event:any){
       console.log(event.target.value);
